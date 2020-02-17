4436a5d2d9c0292bd1a87fddb691729946adb65a
Ben Doerr <ben.doerr@luminsmart.com>
2020-02-17 14:15:00 -0500

    👾👾👾👾

--- chapter_1_5/src/main.rs
+++ chapter_1_5/src/main.rs
@@ -1,4 +1,4 @@
-use rltk::{Console, GameState, Rltk, RGB};
+use rltk::{Console, GameState, Rltk, RGB, Point};
 use specs::prelude::*;
 #[macro_use]
 extern crate specs_derive;
@@ -12,15 +12,23 @@ mod rect;
 pub use rect::*;
 mod visibility_system;
 use visibility_system::VisibilitySystem;
+mod monster_ai_system;
+use monster_ai_system::MonsterAI;
+
+#[derive(PartialEq, Copy, Clone)]
+pub enum RunState { Paused, Running }
 
 pub struct State {
-    ecs: World,
+    pub ecs: World,
+    pub runstate : RunState,
 }
 
 impl State {
     fn run_systems(&mut self) {
         let mut vis = VisibilitySystem {};
         vis.run_now(&self.ecs);
+        let mut mob = MonsterAI{};
+        mob.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -29,17 +37,25 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
 
-        player_input(self, ctx);
+        if self.runstate == RunState::Running {
+            self.run_systems();
+            self.runstate = RunState::Paused
+        } else {
+            self.runstate = player_input(self, ctx);
+        }
 
-        self.run_systems();
 
         draw_map(&self.ecs, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
+        let map = self.ecs.fetch::<Map>();
 
         for (pos, render) in (&positions, &renderables).join() {
-            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+            let idx = map.xy_idx(pos.x, pos.y);
+            if map.visible_tiles[idx] {
+                ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+            }
         }
     }
 }
@@ -47,15 +63,44 @@ impl GameState for State {
 fn main() {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50().with_title("Chapter 1.5").build();
-    let mut gs = State { ecs: World::new() };
+    let mut gs = State { ecs: World::new(), runstate : RunState::Running };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
     gs.ecs.register::<Viewshed>();
+    gs.ecs.register::<Monster>();
+    gs.ecs.register::<Name>();
 
     let map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].center();
+
+    let mut rng = rltk::RandomNumberGenerator::new();
+    for (i, room) in map.rooms.iter().skip(1).enumerate() {
+        let (x,y) = room.center();
+
+        let glyph : u8;
+        let name : String;
+        let roll = rng.roll_dice(1,2);
+        match roll {
+            1 => {glyph = rltk::to_cp437('g'); name = "Goblin".to_string(); }
+            _ => {glyph = rltk::to_cp437('o'); name = "Orc".to_string(); }
+        }
+
+        gs.ecs.create_entity()
+            .with(Position{x,y})
+            .with(Renderable{
+                glyph: glyph,
+                fg: RGB::named(rltk::RED),
+                bg: RGB::named(rltk::BLACK),
+            })
+            .with(Viewshed{visible_tiles: Vec::new(), range: 8, dirty: true})
+            .with(Monster{})
+            .with(Name{name : format!("{} #{}", &name, i)})
+            .build();
+    }
+
     gs.ecs.insert(map);
+    gs.ecs.insert(Point::new(player_x, player_y));
 
     gs.ecs
         .create_entity()
@@ -74,6 +119,7 @@ fn main() {
             range: 8,
             dirty: true,
         })
+        .with(Name{name:"Player".to_string()})
         .build();
 
     rltk::main_loop(context, gs);
